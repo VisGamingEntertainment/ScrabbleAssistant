108c2468c97e97ab8de67f5edf3034591a9ede93
mmgfrcs <viryadharmawan@gmail.com>
2016-04-14 21:04:57 +0700

    Design File Upload
    For those who wanted to see the form designs

---
+++ slns/Form1.Designer.vb
@@ -0,0 +1,494 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Opt1 = New System.Windows.Forms.RadioButton()
+        Me.Opt2 = New System.Windows.Forms.RadioButton()
+        Me.Opt3 = New System.Windows.Forms.RadioButton()
+        Me.Opt4 = New System.Windows.Forms.RadioButton()
+        Me.Opt8 = New System.Windows.Forms.RadioButton()
+        Me.Opt7 = New System.Windows.Forms.RadioButton()
+        Me.Opt6 = New System.Windows.Forms.RadioButton()
+        Me.Opt5 = New System.Windows.Forms.RadioButton()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape3 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape2 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TxtName1 = New System.Windows.Forms.TextBox()
+        Me.LblName1 = New System.Windows.Forms.Label()
+        Me.TxtName2 = New System.Windows.Forms.TextBox()
+        Me.TxtName3 = New System.Windows.Forms.TextBox()
+        Me.TxtName4 = New System.Windows.Forms.TextBox()
+        Me.TxtName5 = New System.Windows.Forms.TextBox()
+        Me.TxtName6 = New System.Windows.Forms.TextBox()
+        Me.TxtName7 = New System.Windows.Forms.TextBox()
+        Me.TxtName8 = New System.Windows.Forms.TextBox()
+        Me.LblName2 = New System.Windows.Forms.Label()
+        Me.LblName3 = New System.Windows.Forms.Label()
+        Me.LblName4 = New System.Windows.Forms.Label()
+        Me.LblName5 = New System.Windows.Forms.Label()
+        Me.LblName6 = New System.Windows.Forms.Label()
+        Me.LblName7 = New System.Windows.Forms.Label()
+        Me.LblName8 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 18)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "No. of Players"
+        '
+        'Opt1
+        '
+        Me.Opt1.AutoSize = True
+        Me.Opt1.Checked = True
+        Me.Opt1.Location = New System.Drawing.Point(106, 8)
+        Me.Opt1.Name = "Opt1"
+        Me.Opt1.Size = New System.Drawing.Size(33, 22)
+        Me.Opt1.TabIndex = 1
+        Me.Opt1.TabStop = True
+        Me.Opt1.Text = "1"
+        Me.Opt1.UseVisualStyleBackColor = True
+        '
+        'Opt2
+        '
+        Me.Opt2.AutoSize = True
+        Me.Opt2.Location = New System.Drawing.Point(145, 8)
+        Me.Opt2.Name = "Opt2"
+        Me.Opt2.Size = New System.Drawing.Size(33, 22)
+        Me.Opt2.TabIndex = 2
+        Me.Opt2.Text = "2"
+        Me.Opt2.UseVisualStyleBackColor = True
+        '
+        'Opt3
+        '
+        Me.Opt3.AutoSize = True
+        Me.Opt3.Location = New System.Drawing.Point(184, 8)
+        Me.Opt3.Name = "Opt3"
+        Me.Opt3.Size = New System.Drawing.Size(33, 22)
+        Me.Opt3.TabIndex = 3
+        Me.Opt3.Text = "3"
+        Me.Opt3.UseVisualStyleBackColor = True
+        '
+        'Opt4
+        '
+        Me.Opt4.AutoSize = True
+        Me.Opt4.Location = New System.Drawing.Point(223, 8)
+        Me.Opt4.Name = "Opt4"
+        Me.Opt4.Size = New System.Drawing.Size(33, 22)
+        Me.Opt4.TabIndex = 4
+        Me.Opt4.Text = "4"
+        Me.Opt4.UseVisualStyleBackColor = True
+        '
+        'Opt8
+        '
+        Me.Opt8.AutoSize = True
+        Me.Opt8.Location = New System.Drawing.Point(379, 8)
+        Me.Opt8.Name = "Opt8"
+        Me.Opt8.Size = New System.Drawing.Size(33, 22)
+        Me.Opt8.TabIndex = 8
+        Me.Opt8.Text = "8"
+        Me.Opt8.UseVisualStyleBackColor = True
+        '
+        'Opt7
+        '
+        Me.Opt7.AutoSize = True
+        Me.Opt7.Location = New System.Drawing.Point(340, 8)
+        Me.Opt7.Name = "Opt7"
+        Me.Opt7.Size = New System.Drawing.Size(33, 22)
+        Me.Opt7.TabIndex = 7
+        Me.Opt7.Text = "7"
+        Me.Opt7.UseVisualStyleBackColor = True
+        '
+        'Opt6
+        '
+        Me.Opt6.AutoSize = True
+        Me.Opt6.Location = New System.Drawing.Point(301, 8)
+        Me.Opt6.Name = "Opt6"
+        Me.Opt6.Size = New System.Drawing.Size(33, 22)
+        Me.Opt6.TabIndex = 6
+        Me.Opt6.Text = "6"
+        Me.Opt6.UseVisualStyleBackColor = True
+        '
+        'Opt5
+        '
+        Me.Opt5.AutoSize = True
+        Me.Opt5.Location = New System.Drawing.Point(262, 8)
+        Me.Opt5.Name = "Opt5"
+        Me.Opt5.Size = New System.Drawing.Size(33, 22)
+        Me.Opt5.TabIndex = 5
+        Me.Opt5.Text = "5"
+        Me.Opt5.UseVisualStyleBackColor = True
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape3, Me.LineShape2, Me.LineShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(838, 360)
+        Me.ShapeContainer1.TabIndex = 9
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape3
+        '
+        Me.LineShape3.Name = "LineShape3"
+        Me.LineShape3.X1 = 427
+        Me.LineShape3.X2 = 427
+        Me.LineShape3.Y1 = 10
+        Me.LineShape3.Y2 = 352
+        '
+        'LineShape2
+        '
+        Me.LineShape2.Name = "LineShape2"
+        Me.LineShape2.X1 = 102
+        Me.LineShape2.X2 = 249
+        Me.LineShape2.Y1 = 35
+        Me.LineShape2.Y2 = 35
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.X1 = 260
+        Me.LineShape1.X2 = 407
+        Me.LineShape1.Y1 = 35
+        Me.LineShape1.Y2 = 35
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(303, 45)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 18)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "Team Play"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(142, 45)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(64, 18)
+        Me.Label3.TabIndex = 11
+        Me.Label3.Text = "Solo Play"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(7, 76)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(93, 18)
+        Me.Label4.TabIndex = 12
+        Me.Label4.Text = "Player Names"
+        '
+        'TxtName1
+        '
+        Me.TxtName1.Location = New System.Drawing.Point(34, 101)
+        Me.TxtName1.Name = "TxtName1"
+        Me.TxtName1.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName1.TabIndex = 13
+        '
+        'LblName1
+        '
+        Me.LblName1.AutoSize = True
+        Me.LblName1.Location = New System.Drawing.Point(13, 104)
+        Me.LblName1.Name = "LblName1"
+        Me.LblName1.Size = New System.Drawing.Size(15, 18)
+        Me.LblName1.TabIndex = 14
+        Me.LblName1.Text = "1"
+        '
+        'TxtName2
+        '
+        Me.TxtName2.Enabled = False
+        Me.TxtName2.Location = New System.Drawing.Point(34, 132)
+        Me.TxtName2.Name = "TxtName2"
+        Me.TxtName2.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName2.TabIndex = 15
+        '
+        'TxtName3
+        '
+        Me.TxtName3.Enabled = False
+        Me.TxtName3.Location = New System.Drawing.Point(34, 163)
+        Me.TxtName3.Name = "TxtName3"
+        Me.TxtName3.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName3.TabIndex = 16
+        '
+        'TxtName4
+        '
+        Me.TxtName4.Enabled = False
+        Me.TxtName4.Location = New System.Drawing.Point(34, 194)
+        Me.TxtName4.Name = "TxtName4"
+        Me.TxtName4.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName4.TabIndex = 17
+        '
+        'TxtName5
+        '
+        Me.TxtName5.Enabled = False
+        Me.TxtName5.Location = New System.Drawing.Point(34, 225)
+        Me.TxtName5.Name = "TxtName5"
+        Me.TxtName5.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName5.TabIndex = 18
+        '
+        'TxtName6
+        '
+        Me.TxtName6.Enabled = False
+        Me.TxtName6.Location = New System.Drawing.Point(34, 256)
+        Me.TxtName6.Name = "TxtName6"
+        Me.TxtName6.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName6.TabIndex = 19
+        '
+        'TxtName7
+        '
+        Me.TxtName7.Enabled = False
+        Me.TxtName7.Location = New System.Drawing.Point(34, 287)
+        Me.TxtName7.Name = "TxtName7"
+        Me.TxtName7.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName7.TabIndex = 20
+        '
+        'TxtName8
+        '
+        Me.TxtName8.Enabled = False
+        Me.TxtName8.Location = New System.Drawing.Point(34, 318)
+        Me.TxtName8.Name = "TxtName8"
+        Me.TxtName8.Size = New System.Drawing.Size(375, 25)
+        Me.TxtName8.TabIndex = 21
+        '
+        'LblName2
+        '
+        Me.LblName2.AutoSize = True
+        Me.LblName2.Enabled = False
+        Me.LblName2.Location = New System.Drawing.Point(13, 135)
+        Me.LblName2.Name = "LblName2"
+        Me.LblName2.Size = New System.Drawing.Size(15, 18)
+        Me.LblName2.TabIndex = 22
+        Me.LblName2.Text = "2"
+        '
+        'LblName3
+        '
+        Me.LblName3.AutoSize = True
+        Me.LblName3.Enabled = False
+        Me.LblName3.Location = New System.Drawing.Point(13, 166)
+        Me.LblName3.Name = "LblName3"
+        Me.LblName3.Size = New System.Drawing.Size(15, 18)
+        Me.LblName3.TabIndex = 23
+        Me.LblName3.Text = "3"
+        '
+        'LblName4
+        '
+        Me.LblName4.AutoSize = True
+        Me.LblName4.Enabled = False
+        Me.LblName4.Location = New System.Drawing.Point(13, 197)
+        Me.LblName4.Name = "LblName4"
+        Me.LblName4.Size = New System.Drawing.Size(15, 18)
+        Me.LblName4.TabIndex = 24
+        Me.LblName4.Text = "4"
+        '
+        'LblName5
+        '
+        Me.LblName5.AutoSize = True
+        Me.LblName5.Enabled = False
+        Me.LblName5.Location = New System.Drawing.Point(13, 228)
+        Me.LblName5.Name = "LblName5"
+        Me.LblName5.Size = New System.Drawing.Size(15, 18)
+        Me.LblName5.TabIndex = 25
+        Me.LblName5.Text = "5"
+        '
+        'LblName6
+        '
+        Me.LblName6.AutoSize = True
+        Me.LblName6.Enabled = False
+        Me.LblName6.Location = New System.Drawing.Point(13, 259)
+        Me.LblName6.Name = "LblName6"
+        Me.LblName6.Size = New System.Drawing.Size(15, 18)
+        Me.LblName6.TabIndex = 26
+        Me.LblName6.Text = "6"
+        '
+        'LblName7
+        '
+        Me.LblName7.AutoSize = True
+        Me.LblName7.Enabled = False
+        Me.LblName7.Location = New System.Drawing.Point(13, 290)
+        Me.LblName7.Name = "LblName7"
+        Me.LblName7.Size = New System.Drawing.Size(15, 18)
+        Me.LblName7.TabIndex = 27
+        Me.LblName7.Text = "7"
+        '
+        'LblName8
+        '
+        Me.LblName8.AutoSize = True
+        Me.LblName8.Enabled = False
+        Me.LblName8.Location = New System.Drawing.Point(13, 321)
+        Me.LblName8.Name = "LblName8"
+        Me.LblName8.Size = New System.Drawing.Size(15, 18)
+        Me.LblName8.TabIndex = 28
+        Me.LblName8.Text = "8"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(442, 12)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(83, 18)
+        Me.Label13.TabIndex = 29
+        Me.Label13.Text = "Game Mode"
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"Classic Scrabble", "Time Attack", "Point Madness", "Challenge Mode"})
+        Me.ComboBox1.Location = New System.Drawing.Point(530, 9)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(296, 26)
+        Me.ComboBox1.TabIndex = 30
+        Me.ComboBox1.Text = "Classic Scrabble"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(445, 45)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(166, 22)
+        Me.CheckBox1.TabIndex = 31
+        Me.CheckBox1.Text = "Enable Scrabble Bonus"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Label14
+        '
+        Me.Label14.Location = New System.Drawing.Point(442, 70)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(384, 238)
+        Me.Label14.TabIndex = 32
+        Me.Label14.Text = "A classic game of Scrabble for everyone which uses the usual rules of Scrabble. N" & _
+    "ot that fun, but it's a good start"
+        '
+        'Button1
+        '
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(728, 313)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(98, 34)
+        Me.Button1.TabIndex = 33
+        Me.Button1.Text = "Start"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.ClientSize = New System.Drawing.Size(838, 360)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label14)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.LblName8)
+        Me.Controls.Add(Me.LblName7)
+        Me.Controls.Add(Me.LblName6)
+        Me.Controls.Add(Me.LblName5)
+        Me.Controls.Add(Me.LblName4)
+        Me.Controls.Add(Me.LblName3)
+        Me.Controls.Add(Me.LblName2)
+        Me.Controls.Add(Me.TxtName8)
+        Me.Controls.Add(Me.TxtName7)
+        Me.Controls.Add(Me.TxtName6)
+        Me.Controls.Add(Me.TxtName5)
+        Me.Controls.Add(Me.TxtName4)
+        Me.Controls.Add(Me.TxtName3)
+        Me.Controls.Add(Me.TxtName2)
+        Me.Controls.Add(Me.LblName1)
+        Me.Controls.Add(Me.TxtName1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Opt8)
+        Me.Controls.Add(Me.Opt7)
+        Me.Controls.Add(Me.Opt6)
+        Me.Controls.Add(Me.Opt5)
+        Me.Controls.Add(Me.Opt4)
+        Me.Controls.Add(Me.Opt3)
+        Me.Controls.Add(Me.Opt2)
+        Me.Controls.Add(Me.Opt1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.Font = New System.Drawing.Font("Calibri", 11.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "First Setup - Scrabble Assistant"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Opt1 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt2 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt3 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt4 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt8 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt7 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt6 As System.Windows.Forms.RadioButton
+    Friend WithEvents Opt5 As System.Windows.Forms.RadioButton
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape3 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape2 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents TxtName1 As System.Windows.Forms.TextBox
+    Friend WithEvents LblName1 As System.Windows.Forms.Label
+    Friend WithEvents TxtName2 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName3 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName4 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName5 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName6 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName7 As System.Windows.Forms.TextBox
+    Friend WithEvents TxtName8 As System.Windows.Forms.TextBox
+    Friend WithEvents LblName2 As System.Windows.Forms.Label
+    Friend WithEvents LblName3 As System.Windows.Forms.Label
+    Friend WithEvents LblName4 As System.Windows.Forms.Label
+    Friend WithEvents LblName5 As System.Windows.Forms.Label
+    Friend WithEvents LblName6 As System.Windows.Forms.Label
+    Friend WithEvents LblName7 As System.Windows.Forms.Label
+    Friend WithEvents LblName8 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
