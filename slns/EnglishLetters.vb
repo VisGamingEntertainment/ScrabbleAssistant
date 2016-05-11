86fb9441d5dc6b81dd2fa6046477f6e559e98b38
Virya Dharmawan <viryadharmawan@gmail.com>
2016-05-11 13:04:00 +0700

    Letters and Words Update
    Completing the list of English letters and some common low-count, high-scoring words

--- slns/EnglishLetters.vb
+++ slns/EnglishLetters.vb
@@ -1,13 +1,69 @@
-Namespace EnglishLetters
-  Public Class Letters
+Namespace WordsLetters
+  Public Class EnglishLetters
+  'TODO: Include letters of other languages similar to English
     Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
-    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_B_VALUE As Integer = 3
+    Public Const LETTER_C_VALUE As Integer = 3
+    Public Const LETTER_D_VALUE As Integer = 2
+    Public Const LETTER_E_VALUE As Integer = 1
+    Public Const LETTER_F_VALUE As Integer = 4
+    Public Const LETTER_G_VALUE As Integer = 2
+    Public Const LETTER_H_VALUE As Integer = 4
+    Public Const LETTER_I_VALUE As Integer = 1
+    Public Const LETTER_J_VALUE As Integer = 8
+    Public Const LETTER_K_VALUE As Integer = 5
+    Public Const LETTER_L_VALUE As Integer = 1
+    Public Const LETTER_M_VALUE As Integer = 3
+    Public Const LETTER_N_VALUE As Integer = 1
+    Public Const LETTER_O_VALUE As Integer = 1
+    Public Const LETTER_P_VALUE As Integer = 3
+    Public Const LETTER_Q_VALUE As Integer = 10
+    Public Const LETTER_R_VALUE As Integer = 1
+    Public Const LETTER_S_VALUE As Integer = 1
+    Public Const LETTER_T_VALUE As Integer = 1
+    Public Const LETTER_U_VALUE As Integer = 1
+    Public Const LETTER_V_VALUE As Integer = 4
+    Public Const LETTER_W_VALUE As Integer = 4
+    Public Const LETTER_X_VALUE As Integer = 8
+    Public Const LETTER_Y_VALUE As Integer = 4
+    Public Const LETTER_Z_VALUE As Integer = 10
+    Public Const LETTER_BLANK_VALUE As Integer = 0
+    
+    Public Const LETTER_A_QUANTITY As Integer = 9
+    Public Const LETTER_B_QUANTITY As Integer = 2
+    Public Const LETTER_C_QUANTITY As Integer = 2
+    Public Const LETTER_D_QUANTITY As Integer = 4
+    Public Const LETTER_E_QUANTITY As Integer = 12
+    Public Const LETTER_F_QUANTITY As Integer = 2
+    Public Const LETTER_G_QUANTITY As Integer = 3
+    Public Const LETTER_H_QUANTITY As Integer = 2
+    Public Const LETTER_I_QUANTITY As Integer = 9
+    Public Const LETTER_J_QUANTITY As Integer = 1
+    Public Const LETTER_K_QUANTITY As Integer = 1
+    Public Const LETTER_L_QUANTITY As Integer = 4
+    Public Const LETTER_M_QUANTITY As Integer = 2
+    Public Const LETTER_N_QUANTITY As Integer = 6
+    Public Const LETTER_O_QUANTITY As Integer = 8
+    Public Const LETTER_P_QUANTITY As Integer = 2
+    Public Const LETTER_Q_QUANTITY As Integer = 1
+    Public Const LETTER_R_QUANTITY As Integer = 6
+    Public Const LETTER_S_QUANTITY As Integer = 4
+    Public Const LETTER_T_QUANTITY As Integer = 6
+    Public Const LETTER_U_QUANTITY As Integer = 4
+    Public Const LETTER_V_QUANTITY As Integer = 2
+    Public Const LETTER_W_QUANTITY As Integer = 2
+    Public Const LETTER_X_QUANTITY As Integer = 1
+    Public Const LETTER_Y_QUANTITY As Integer = 2
+    Public Const LETTER_Z_QUANTITY As Integer = 1
+    Public Const LETTER_BLANK_QUANTITY As Integer = 2
+
+  End Class
+  Public Class WordDictionary
+  'For the Score Madness Mode. Can be implemented for the Classic Mode
+  'This class stored all words that are too common to worth points, modifying the point values of those words by the one listed below.
+  'TODO: Complete this, of course
+    Public Const WORD_OZ_VALUE As Integer = 4
+    Public Const WORD_XOR_VALUE As Integer = 5
+    Public Const WORD_OX_VALUE As Integer = 4
   End Class
 End Module
