b0f7e1f3822e90355b501b23aa4874717989bf6c
Virya Dharmawan <viryadharmawan@gmail.com>
2016-05-11 12:02:45 +0700

    Create EnglishLetters.vb

---
+++ slns/EnglishLetters.vb
@@ -0,0 +1,13 @@
+Namespace EnglishLetters
+  Public Class Letters
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+    Public Const LETTER_A_VALUE As Integer = 1
+  End Class
+End Module
